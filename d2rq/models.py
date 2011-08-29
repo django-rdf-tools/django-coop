6053b1e8f5b826d61e999002f18d97d4ed8a29cb
Dominique Guardiola <contact@quinode.fr>
2011-08-29 12:40:20 +0200

    First draft of the vocabulary fetching and storing - requires librdf + python bindings

--- d2rq/models.py
+++ d2rq/models.py
@@ -1,12 +1,16 @@
 # -*- coding:utf-8 -*-
 from django.db import models
+from django_extensions.db import fields as exfields
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
-import config #unused but needed by livesettings !
+import config # not unused, needed by livesettings !
 import surf, rdflib
+from surf import ns
 from surf.query import select
 from urllib2 import Request, urlopen, URLError, HTTPError
-
+#from djutils.decorators import async
+import settings
+import re
 
 def literal_lang_select(list):
     '''
@@ -15,130 +19,219 @@ def literal_lang_select(list):
     result = None
     pref_lang = 'fr' #TODO livesettings
     fallback = 'en'
-    for lit in list:
-        if(lit.language == pref_lang):
-            result = unicode(lit)
+    for literal in list:
+        if(literal.language == pref_lang):
+            result = unicode(literal)
             break
-        elif(lit.language == fallback):
-            result = unicode(lit)
+        elif(literal.language == fallback):
+            result = unicode(literal)
         elif(not result):
-            result = unicode(lit)
-    return(result)        
+            result = unicode(literal)
+    return(' '.join(result.split()))        
 
 
-def get_schema_label(uri):
+#@async    
+def download_schema(uri, prefix):
+    headers = { "Accept": "text/turtle,text/n3,text/rdf+n3,application/rdf+xml,text/rdf,text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5",
+                "Accept-Language": "en-us,en;q=0.5",
+                "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.7",
+                "User-Agent": "django-d2rq"
+                }
+    req = Request(uri,None,headers)
+    try:
+        handle = urlopen(req)
+    except URLError, e:
+        print 'La connexion avec le serveur a échoué.'
+        print 'Raison: ', e.reason
+    except HTTPError, e:
+        print 'Erreur du serveur.'
+        print 'Code : ', e.code
+    format = handle.info().gettype()
+    formats = { 'application/rdf+xml':'rdf','text/turtle' : 'ttl',
+                'text/n3' :'n3','text/rdf+n3':'n3','text/rdf':'rdf',
+                'text/xml':'xml','application/xml':'xml'}
+    extension = formats[format]
+    filename = 'd2rq/vocab/'+prefix+'.'+extension
+    local_file = open(filename, "w")
+    local_file.write(handle.read())
+    local_file.close()
+    return extension
+
+
+def get_label(subject,store):
+    label = None
+    ns_titles = [ns.RDFS["comment"],ns.RDFS["label"],ns.DC["title"],ns.DCTERMS["title"]]
+    for title in ns_titles:
+        query = select("?o").where((subject,title,"?o"))
+        found_title = list(store.reader._to_table(store.reader._execute(query)))
+        #print 'Nombre de labels '+title+' :'+str(len(found_title))
+        if(len(found_title)>0):
+            label = literal_lang_select([found_title[x]['o'] for x in range(len(found_title))])
+    if(label==None): #on n'a rien trouvé du tout
+        label = unicode(subject)
+    return label    
+
+
+def get_schema_label(uri,prefix,format):
     #store = surf.Store(reader='rdflib',writer='rdflib',rdflib_store='IOMemory')
     store = surf.Store(**{"reader": "librdf", "writer" : "librdf", })
     session = surf.Session(store)
-    store.load_triples(source=uri)
+    try:
+        store.load_triples(source='file://'+settings.PROJECT_PATH+'/d2rq/vocab/'+prefix+'.'+format)
+        print "Loading from file : "+prefix+"."+format
+    except:
+        store.load_triples(source=uri)
+        print "Loading from Internet : "+uri
     #store.enable_logging(True)
-    ownuri = rdflib.term.URIRef(uri)
-    ontology = session.get_class(surf.ns.OWL.Ontology)
+    ontology = session.get_class(ns.OWL.Ontology)
     if(ontology.all().first()): #contient un node Ontology
-        titles = ['rdfs_label','dc_title','dcterms_title']
+        print "Le schema est une ontologie OWL"
+        vocab_type = 'owl'
+        titles = ['rdfs_comment','rdfs_label','dc_title','dcterms_title']
+        #import pdb; pdb.set_trace() 
         for o in ontology.all():
             for title in titles:
-                lookup = literal_lang_select(o.__getattr__(title))
-                if(lookup != None):label=lookup
-            if(not label):
+                if(len(o.__getattr__(title))>0):
+                    lookup = literal_lang_select(o.__getattr__(title))
+                    if(lookup != None):label=lookup
+            if not label:
                 label = unicode(uri)
-    else: #ce n'est pas une ontologie OWL, idem mais autre méthode , orientée query
-    # cette méthode pourrait fonctionner pour les ontologies aussi, sauf quand
-    #(pb 303 ou autre) l'URI sujet est differente de l'URI stockée...
-        ns_titles = [surf.ns.RDFS["label"],surf.ns.DC["title"],surf.ns.DCTERMS["title"]]
-        for title in ns_titles:
-            query = select("?o").where((ownuri,title,"?o"))
-            found_title = list(store.reader._to_table(store.reader._execute(query)))
-            if(len(found_title)==1):
-                label = literal_lang_select([found_title[0]['o']])
-        if(not label): #on n'a rien trouvé avec la méthode vocab
-            label = unicode(uri)
-    #import pdb; pdb.set_trace()                
+    else: # là c'est la vraie methode, ci-dessus c'est pour les OWL dont l'URL ≠ de l'URI sujet
+        try:
+            print "Le schema est un vocabulaire RDFS"
+            vocab_type = 'rdfs'
+            label = get_label(rdflib.term.URIRef(uri),store)
+        except:
+            raise   
+        # ns_titles = [ns.RDFS["comment"],ns.RDFS["label"],ns.DC["title"],ns.DCTERMS["title"]]
+        #         for title in ns_titles:
+        #             query = select("?o").where((ownuri,title,"?o"))
+        #             found_title = list(store.reader._to_table(store.reader._execute(query)))
+        #             if(len(found_title)>0):
+        #                 label = literal_lang_select([found_title[x]['o'] for x in range(len(found_title))])
+        #         if(not label): #on n'a rien trouvé avec la méthode rfds non plus
+        #             label = unicode(uri)
+    cp = {'classes':[],'proprietes':[]}
+    if(vocab_type=='owl'):#TODO FILTER : on veut que ses propres classes !
+        #vocab_classes = session.get_class(ns.OWL.Class).all()
+        vocab_classes = list(store.reader._to_table(store.reader._execute(
+                            select("?s").where(("?s",ns.RDF.type,ns.OWL.Class)))))
+        vocab_proprietes = list(store.reader._to_table(store.reader._execute(
+                            select("?s").where(("?s",ns.RDF.type,ns.OWL.ObjectProperty)))))        
+    elif(vocab_type=='rdfs'):
+        vocab_classes = list(store.reader._to_table(store.reader._execute(
+                            select("?s").where(("?s",ns.RDF.type,ns.RDFS.Class)))))
+        vocab_proprietes = list(store.reader._to_table(store.reader._execute(
+                            select("?s").where(("?s",ns.RDF.type,ns.RDF.Property)))))
+    for items in ((vocab_classes,'classes'),(vocab_proprietes,'proprietes')):
+        for triple in items[0]:
+            if(isinstance(triple['s'],rdflib.term.URIRef) and bool(re.compile(uri).match(unicode(triple['s'])))): #ugly patch waiting for query to work
+                clabel = get_label(triple['s'],store) #on envoie le subject pour l'identifier
+                cp[items[1]].append((unicode(triple['s']),clabel))
+                print items[1]+':'+clabel
     store.clear()
     store.close()
     session.close()
-    return(label)
-
-
+    return((label,vocab_type,cp))
 
 
 def update_class_and_properties(schema):
     pass
 
+
 class Schema(models.Model):
     prefix = models.CharField(_(u'Préfixe du schéma'),max_length=10)
     uri = models.CharField(_(u'URI du schéma'),max_length=200) #TODO URIField that does not cut "#" at the end would be cool
     label = models.CharField(_(u'Intitulé'),max_length=200,blank=True,editable=False)
+    format = models.CharField(blank=True, max_length=5,editable=False)
+    vocab_type = models.CharField(blank=True, max_length=5,editable=False)
     def __unicode__(self):
         return unicode(self.label)
     def link_in_admin(self):
         return '<a target="_blank" href="%s">%s</a>' % (self.uri, self.uri)
-    link_in_admin.allow_tags = True
-        
-    def download_schema(self):
-        headers = { "Accept": "text/turtle,text/n3,text/rdf+n3,application/rdf+xml,text/rdf,text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5",
-                    "Accept-Language": "en-us,en;q=0.5",
-                    "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.7",
-                    "User-Agent": "django-d2rq"
-                    }
-        req = Request(self.uri,None,headers)
-        try:
-            handle = urlopen(req)
-        except URLError, e:
-            print 'La connexion avec le serveur a échoué.'
-            print 'Raison: ', e.reason
-        except HTTPError, e:
-            print 'Erreur du serveur.'
-            print 'Code : ', e.code
-        format = handle.info().gettype()
-        formats = { 'application/rdf+xml':'rdf','text/turtle' : 'ttl',
-                    'text/n3' :'n3','text/rdf+n3':'n3','text/rdf':'rdf',
-                    'text/xml':'rdf','application/xml':'rdf'}
-        filename = 'd2rq/vocab/'+self.prefix+'.'+formats[format]
-        local_file = open(filename, "w")
-        local_file.write(handle.read())
-        local_file.close()    
-
-    def save(self, *args, **kwargs):
-        self.download_schema()
-        try:
-            self.label = get_schema_label(self.uri)
-        except:
-            self.label = self.uri           
+    link_in_admin.allow_tags = True   
+    def save(self, *args, **kwargs):        
+        self.format = download_schema(self.uri,self.prefix)
+        print 'Serialisation du schema : '+self.format
+        #exception : pas téléchargeable
+        if(self.format in ['rdf','n3','ttl']):
+            try:
+                self.label, self.vocab_type,cp = get_schema_label(self.uri,self.prefix,self.format)
+                print 'Titre : "'+self.label+'", Type de schema : '+self.vocab_type
+            except:
+                raise
+        else:
+            self.label = self.uri
+            self.vocab_type = '???'   
         super(Schema, self).save(*args, **kwargs)
-        update_class_and_properties(self)
+        # update_class_and_properties(self)
+        if('cp' in locals()):
+            SchemaClass.objects.filter(schema=self).delete()
+            SchemaProperty.objects.filter(schema=self).delete()
+            for sc in cp['classes']:
+                sc = SchemaClass(schema=self,class_name=sc[0],class_label=sc[1])
+                sc.save()
+            for sp in cp['proprietes']:
+                sp = SchemaProperty(schema=self,prop_name=sp[0],prop_label=sp[1])
+                sp.save()
+        
+
 
 class SchemaClass(models.Model):
     schema = models.ForeignKey(Schema)
+    class_name = models.CharField(_(u'Classe RDF'),max_length=250,unique=True)
     class_label = models.CharField(_(u'Libellé de classe RDF'),max_length=250)
-    class_name = models.CharField(_(u'Classe RDF'),max_length=250)
+    def __unicode__(self):
+        return(self.class_label)
+    # class Meta:
+    #     ordering = 'class_label'
+    #     verbose_name = _(u'Classe RDF')
+    #     verbose_name_plural = _(u'Classes RDF')
+
 
 class SchemaProperty(models.Model):
     schema = models.ForeignKey(Schema)
+    prop_name = models.CharField(_(u'Propriété RDF'),max_length=250,unique=True)
     prop_label = models.CharField(_(u'Libellé de propriété RDF'),max_length=250)
-    prop_name = models.CharField(_(u'Propriété RDF'),max_length=250)
+    def __unicode__(self):
+        return(self.prop_label)
+    # class Meta:
+    #     ordering = 'prop_label'
+    #     verbose_name = _(u'Propriété RDF')
+    #     verbose_name_plural = _(u'Propriétés RDF')        
 
 
 from livesettings import config_value
-
 done = []
 AVAILABLE_MODELS = []
 all_models = ContentType.objects.filter(app_label__in=config_value('d2rq','MAPPED_APPS')).order_by('app_label')
 for a in all_models:
     if(a.app_label not in done):
         AVAILABLE_MODELS.append((
-            a.app_label , tuple((x.model,x.name) for x in all_models.filter(app_label=a.app_label))
+            a.app_label , 
+            tuple((x.model,x.name) for x in all_models.filter(app_label=a.app_label))
             ))
     done.append(a.app_label)        
+
+
+AVAILABLE_CLASSES = []
+for s in Schema.objects.all().order_by('prefix'):
+    AVAILABLE_CLASSES.append((
+        s.prefix+':', 
+        tuple((x.class_name,x.class_label) for x in SchemaClass.objects.filter(schema=s))
+    ))
 
 
 class MappedModel(models.Model):
     model_name = models.CharField(_(u'Modéle mappé'),max_length=200,choices=AVAILABLE_MODELS,null=True)
     #model_name = models.ForeignKey(ContentType,to_field='model',verbose_name=_(u'Modéle mappé'))
-    rdf_class = models.ForeignKey(SchemaClass,verbose_name=_(u'Classe RDF'),null=True) 
+    rdf_class = models.ForeignKey(SchemaClass,verbose_name=_(u'Classe RDF'),choices=AVAILABLE_CLASSES,null=True) 
+    classMap_label = exfields.AutoSlugField('d2rq:classDefinitionLabel',populate_from=('model_name'))
+
 
 class MappedField(models.Model):
     model = models.ForeignKey(MappedModel)
     field_name = models.TextField(_(u'Champ mappé'),null=True)
     rdf_proprety = models.ForeignKey(SchemaProperty,verbose_name=_(u'Propriété RDF'),null=True)
-    
+
+   
