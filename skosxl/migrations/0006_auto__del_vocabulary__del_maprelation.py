9203933c663371cc8744d2a9b9d6ad1a34504f1a
Dominique Guardiola <contact@quinode.fr>
2011-12-16 20:02:10 +0100

    skoxl refactoring

---
+++ skosxl/migrations/0006_auto__del_vocabulary__del_maprelation.py
@@ -0,0 +1,131 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Deleting model 'Vocabulary'
+        db.delete_table('skosxl_vocabulary')
+
+        # Deleting model 'MapRelation'
+        db.delete_table('skosxl_maprelation')
+
+
+    def backwards(self, orm):
+        
+        # Adding model 'Vocabulary'
+        db.create_table('skosxl_vocabulary', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('info_url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal('skosxl', ['Vocabulary'])
+
+        # Adding model 'MapRelation'
+        db.create_table('skosxl_maprelation', (
+            ('target_label', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('voc', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['skosxl.Vocabulary'])),
+            ('uri', self.gf('django.db.models.fields.CharField')(max_length=250)),
+            ('origin_concept', self.gf('django.db.models.fields.related.ForeignKey')(related_name='map_origin', to=orm['skosxl.Concept'])),
+            ('match_type', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=0)),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        ))
+        db.send_create_signal('skosxl', ['MapRelation'])
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'skosxl.concept': {
+            'Meta': {'object_name': 'Concept'},
+            'author_uri': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'changenote': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'definition': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'pref_label': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'scheme': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['skosxl.Scheme']", 'null': 'True', 'blank': 'True'}),
+            'sem_relations': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['skosxl.Concept']", 'through': "orm['skosxl.SemRelation']", 'symmetrical': 'False'}),
+            'status': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0'}),
+            'top_concept': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'uri': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        },
+        'skosxl.label': {
+            'Meta': {'object_name': 'Label'},
+            'concept': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'labels'", 'to': "orm['skosxl.Concept']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'term': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['skosxl.Term']"}),
+            'type': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0'})
+        },
+        'skosxl.scheme': {
+            'Meta': {'object_name': 'Scheme'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'blank': 'True'}),
+            'pref_label': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'uri': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'})
+        },
+        'skosxl.semrelation': {
+            'Meta': {'object_name': 'SemRelation'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'origin_concept': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'rel_origin'", 'to': "orm['skosxl.Concept']"}),
+            'target_concept': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'rel_target'", 'to': "orm['skosxl.Concept']"}),
+            'type': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'})
+        },
+        'skosxl.term': {
+            'Meta': {'object_name': 'Term'},
+            'author_uri': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'concept': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['skosxl.Concept']", 'through': "orm['skosxl.Label']", 'symmetrical': 'False'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'default': "'fr'", 'max_length': '10'}),
+            'literal': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'occurences': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'db_index': 'True', 'max_length': '50', 'blank': 'True'}),
+            'uri': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['skosxl']
