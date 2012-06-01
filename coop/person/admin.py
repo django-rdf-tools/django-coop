5fe3f549adb29cf52b00a75f24cea5fb57b7e0d2
Dominique Guardiola <dguardiola@quinode.fr>
2012-06-01 13:45:22 +0200

    default fieldsets for person admin

--- coop/person/admin.py
+++ coop/person/admin.py
@@ -9,7 +9,6 @@ from django_extensions.admin import ForeignKeyAutocompleteAdmin
 
 
 class PersonAdminForm(forms.ModelForm):
-    change_form_template = 'admintools_bootstrap/tabbed_change_form.html' 
     # category = chosenforms.ChosenModelMultipleChoiceField(
     #         required=False,
     #         overlay=_(u"Choose one or more categories"),
@@ -20,6 +19,7 @@ class PersonAdminForm(forms.ModelForm):
 
 
 class PersonAdmin(ForeignKeyAutocompleteAdmin):  # FkAutocompleteAdmin too but...
+    change_form_template = 'admintools_bootstrap/tabbed_change_form.html' 
     form = PersonAdminForm
     search_fields = ['last_name', 'first_name', 'email']
     list_display = ('last_name', 'first_name', 'email', 'structure', 'has_user_account', 'has_role')
@@ -27,8 +27,7 @@ class PersonAdmin(ForeignKeyAutocompleteAdmin):  # FkAutocompleteAdmin too but..
     list_display_links = ('last_name', 'first_name')
     search_fields = ('last_name', 'first_name', 'email', 'structure')    
     ordering = ('last_name',)
-    inlines = [
-                ContactInline,
+    inlines = [ ContactInline,
                 OrgInline,
                 ]
 
@@ -37,14 +36,14 @@ class PersonAdmin(ForeignKeyAutocompleteAdmin):  # FkAutocompleteAdmin too but..
         return dict(myactions, **super(PersonAdmin, self).get_actions(request))  # merging two dicts
     
     fieldsets = (
-        (None, {
+        ('Identification', {
             'fields': (('first_name', 'last_name'),
+                        ('location', 'location_display'),  # Using coop-geo
                         'email',
                         'category'
                         ),
             }),
         ('Notes', {
-            'classes': ('collapse',),
             'fields': ('structure', 'notes',)
         })
     )
