7be8c8782348caabc9b371337fd6dd95c8c835ed
Claude Huchet <claude.huchet@quinode.fr>
2012-06-05 11:04:28 +0200

    settings

---
+++ coop/settings.py
@@ -0,0 +1,21 @@
+DEFAULT_RDF_NAMESPACES = [
+ ('ess', u'http://ns.economie-solidaire.fr/ess#'),
+ ('dct', u'http://purl.org/dc/terms/'),
+ ('org', u'http://www.w3.org/ns/org#'),
+ ('ctag', u'http://commontag.org/ns#'),
+ ('foaf', u'http://xmlns.com/foaf/0.1/'),
+ ('locn', u'http://www.w3.org/ns/locn#'),
+ ('skos', u'http://www.w3.org/2004/02/skos/core#'),
+ ('legal', u'http://www.w3.org/ns/legal#'),
+ ('v', u'http://www.w3.org/2006/vcard/ns#'),
+ ('xsd', 'http://www.w3.org/2001/XMLSchema#'),
+ ('insee', u'http://rdf.insee.fr/geo/2011/'),
+ ('skosxl', u'http://www.w3.org/2008/05/skos-xl#'),
+ ('gr', u'http://purl.org/goodrelations/v1#'),
+ ('event', u'http://purl.org/NET/c4dm/event.owl#'),
+ ('sioc', u'http://rdfs.org/sioc/ns#'),
+ ('opens', u'http://rdf.opensahara.com/type/geo/'),
+ ('person', u'http://www.w3.org/ns/person#'),
+ ('schema', u'http://schema.org/'),
+ ('rss', u'http://purl.org/net/rss1.1#')
+ ]
