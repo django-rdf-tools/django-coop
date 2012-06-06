493eef2f3ba9038a5e57e7eb1dfb17aca2444d14
Dominique Guardiola <dguardiola@quinode.fr>
2012-06-06 14:38:31 +0200

    slash

--- coop/urls.py
+++ coop/urls.py
@@ -22,7 +22,7 @@ urlpatterns = patterns('',
     (r'^perso/$', 'coop.person.views.perso'),
     (r'^membre/', include('coop.person.urls')),
 
-    url(r'^feed/(?P<model>[\w-]+)$', UpdateFeed()),
+    url(r'^feed/(?P<model>[\w-]+)/$', UpdateFeed()),
 
     url(r'^robots\.txt$', TextPlainView.as_view(template_name='robots.txt')),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico')),
