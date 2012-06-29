ee285fe9f7417469da2f472df354d6c5e0910fbd
Claude Huchet <claude.huchet@quinode.fr>
2012-06-29 15:12:03 +0200

    go backward from rq. Coop applications do not process subhub maintenance task automatically

--- coop/signals.py
+++ coop/signals.py
@@ -31,14 +31,15 @@ def post_save_callback(sender, instance, **kwargs):
         feed_url = 'http://%s/feed/%s/' % (Site.objects.get_current().domain, sender.__name__.lower())
         # print "publish for topic with feed %s  and instance %s" % (feed_url, instance)
         subhub.publish([feed_url], instance.get_absolute_url(), False)
-        q.enqueue(letsCallDistributionTaskProcess)
-    elif isinstance(instance, subhub.models.SubscriptionTask):
-        # call the maintenance
-            print "Call the maintenance"
-            try:
-                log.info('Processing verification queue...')
-                subhub.models.SubscriptionTask.objects.process(log=log)
-            except subhub.utils.LockError, e:
-                log.warning(str(e))
-    else:
-        pass
+    # Lets wait a little bit.....
+    #     q.enqueue(letsCallDistributionTaskProcess)
+    # elif isinstance(instance, subhub.models.SubscriptionTask):
+    #     # call the maintenance
+    #         print "Call the maintenance"
+    #         try:
+    #             log.info('Processing verification queue...')
+    #             subhub.models.SubscriptionTask.objects.process(log=log)
+    #         except subhub.utils.LockError, e:
+    #             log.warning(str(e))
+    # else:
+    #     pass
