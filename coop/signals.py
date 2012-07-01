11e97a229ecb752b48e1a44505b71f5258db70f8
Dominique Guardiola <dguardiola@quinode.fr>
2012-07-01 17:26:53 +0200

    try/pass if redis queue is here

--- coop/signals.py
+++ coop/signals.py
@@ -31,7 +31,10 @@ def post_save_callback(sender, instance, **kwargs):
         feed_url = 'http://%s/feed/%s/' % (Site.objects.get_current().domain, sender.__name__.lower())
         # print "publish for topic with feed %s  and instance %s" % (feed_url, instance)
         subhub.publish([feed_url], instance.get_absolute_url(), False)
-        q.enqueue(letsCallDistributionTaskProcess)
+        try:
+            q.enqueue(letsCallDistributionTaskProcess)
+        except:
+            pass
     elif isinstance(instance, subhub.models.SubscriptionTask):
         # call the maintenance
             print "Call the maintenance"
