d9faa51ae6b5b2b56ebf0f6e28b545ff921b8bd3
Claude Huchet <claude.huchet@quinode.fr>
2012-08-23 16:13:10 +0200

    bye bye mptt

--- coop/base_admin.py
+++ coop/base_admin.py
@@ -5,7 +5,6 @@ from coop_local.models import *
 from coop.person.admin import PersonAdmin
 from coop.exchange.admin import ExchangeAdmin
 from coop.org.admin import OrganizationAdmin
-from coop.tag.admin import CoopTagTreeAdmin
 from coop_tag.settings import get_class
 
 # -- Loading base models
@@ -14,7 +13,6 @@ admin.site.register(LinkProperty)
 
 admin.site.register(PersonCategory)
 admin.site.register(Person, PersonAdmin)
-admin.site.register(Role, CoopTagTreeAdmin)
 
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(OrganizationCategory)
@@ -24,4 +22,4 @@ admin.site.register(Exchange, ExchangeAdmin)
 
 
 admin.site.unregister(get_class('tag'))
-admin.site.register(get_class('tag'), CoopTagTreeAdmin)
+admin.site.register(get_class('tag'))
