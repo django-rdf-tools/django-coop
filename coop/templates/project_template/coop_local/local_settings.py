a6481957c4dfa36c3882a92a2208db6be0ab6d21
Claude Huchet <claude.huchet@quinode.fr>
2012-08-28 12:25:35 +0200

    A settings was missing... la suite

--- coop/templates/project_template/coop_local/local_settings.py
+++ coop/templates/project_template/coop_local/local_settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 SEND_BROKEN_LINK_EMAILS = True
 INTERNAL_IPS = ('127.0.0.1', '92.243.30.98')
 
-PES_HOST ='pes.domain.com'
+PES_HOST = 'pes.domain.com'
 
 from django.conf import settings
 
