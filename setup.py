d4c3a13c9e8fcfa982fdc763652307a7ba9a59a6
Dominique Guardiola <dguardiola@quinode.fr>
2012-09-10 17:02:19 +0200

    manquait tag dans setup

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
                 'coop.agenda',
                 'coop.agenda.conf',
                 'coop.person',
+                'coop.tag',
                 'coop.templatetags',
                 'coop.utils',
                 'coop.webid',
