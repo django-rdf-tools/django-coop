eb7a9c613dd5ca03586ec7c5b2043a32c5975286
Claude Huchet <claude.huchet@quinode.fr>
2013-01-29 14:14:31 +0100

    fix event_type

--- coop/agenda/models.py
+++ coop/agenda/models.py
@@ -95,7 +95,7 @@ class BaseEvent(URIModel):
     slug = exfields.AutoSlugField(populate_from='title')
     category = models.ManyToManyField('coop_local.EventCategory', verbose_name=_('event type'))  
     # to be deleted one day...use category instead
-    event_type = models.ForeignKey('coop_local.EventCategory', verbose_name=_('event type'), related_name='event_type', editable=False)  
+    event_type = models.ForeignKey('coop_local.EventCategory', verbose_name=_('event type'), related_name='event_type', editable=False, null=True)  
     calendar = models.ForeignKey('coop_local.Calendar', verbose_name=_('calendar'))
 
     # Linking to local objects
@@ -149,7 +149,7 @@ class BaseEvent(URIModel):
                 related_name='pref_address_event', null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        if self.event_type:
+        if self.event_type_id:
             self.category.add(self.event_type)
         # try hard to set default value for postal address
         if 'coop_geo' in settings.INSTALLED_APPS:
