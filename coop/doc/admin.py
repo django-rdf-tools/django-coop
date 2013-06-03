c02f6b7a3b59a859186c61d0b200028cbb355efe
Dominique Guardiola <dguardiola@quinode.fr>
2013-06-03 17:28:52 +0200

    remove ISBNWIdget for now

--- coop/doc/admin.py
+++ coop/doc/admin.py
@@ -19,27 +19,27 @@ from coop.link.admin import LinksInline
 from django.db.models import URLField
 from django.contrib.admin.widgets import AdminURLFieldWidget
 from django.utils.safestring import mark_safe
-import pyisbn
+# import pyisbn
 from coop.doc.models import ISBNField
 from chosen import widgets as chosenwidgets
 
-class ISBNWidget(forms.TextInput):
-    def __init__(self, language=None, attrs={}):
-        super(ISBNWidget, self).__init__(attrs=attrs)
+# class ISBNWidget(forms.TextInput):
+#     def __init__(self, language=None, attrs={}):
+#         super(ISBNWidget, self).__init__(attrs=attrs)
 
-    def render(self, name, value, attrs={}):
-        if (not value) or len(str(value)) < 2:
-            return super(ISBNWidget, self).render(name, value, attrs)
-        return super(ISBNWidget, self).render(name, value, attrs) + mark_safe("""
-            <br />
-            <div class="alt">%s-digit: <span class="altisbn">%s <span class="doodad">
-                <a href="http://www.decitre.fr/livres/%s.html"> Voir sur Decitre &#9901;</a>
-            </span></span></div>
-        """ % (
-            (len(str(value)) == 10) and "13" or "10",
-            pyisbn.convert(value),
-            value
-        ))
+#     def render(self, name, value, attrs={}):
+#         if (not value) or len(str(value)) < 2:
+#             return super(ISBNWidget, self).render(name, value, attrs)
+#         return super(ISBNWidget, self).render(name, value, attrs) + mark_safe("""
+#             <br />
+#             <div class="alt">%s-digit: <span class="altisbn">%s <span class="doodad">
+#                 <a href="http://www.decitre.fr/livres/%s.html"> Voir sur Decitre &#9901;</a>
+#             </span></span></div>
+#         """ % (
+#             (len(str(value)) == 10) and "13" or "10",
+#             pyisbn.convert(value),
+#             value
+#         ))
 
 
 # obligé de re-déclarer cette classe de coop.org.admin a cause d'une boucle d'import
